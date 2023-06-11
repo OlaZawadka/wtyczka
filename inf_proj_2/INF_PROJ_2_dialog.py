daf8daceace2c7eb7c3bba7b1b07c940789eab3e
PaulinaMMMMMMMMM <paula.oyama1@gmail.com>
2023-06-11 15:24:00 +0200

    Update INF_PROJ_2_dialog.py

--- inf_proj_2/INF_PROJ_2_dialog.py
+++ inf_proj_2/INF_PROJ_2_dialog.py
@@ -91,5 +91,5 @@ class INF_PROJ_2Dialog(QtWidgets.QDialog, FORM_CLASS):
                 P = 0.5 * np.abs(np.dot(X, np.roll(Y, 1)) - np.dot(Y, np.roll(X, 1)))
                 self.label_wynik.setText(f'Pole powierzchni między punktami {Numer[0]} , {Numer[1]} i {Numer[2]} wynosi: {P:.3f} m^2')
             else:
-                self.label_wynik.setText('Error: Nie wybrano wystarczającej liczby punktów.')
+                self.label_wynik.setText('Error: Nieodpowiednia liczba punktów.')
        
